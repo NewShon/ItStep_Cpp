5bfbee1a8a69f63f4b948cdf0dc55219356be938
NewShon <kirill31_95@mail.ru>
2017-06-10 22:13:20 +0300

    Add files via upload

---
+++ lab6/ex1/Digit.cpp
@@ -0,0 +1,200 @@
+#include "Digit.h"
+
+unsigned int Digit::counter = 0;
+
+Digit::Digit() : digit(0) { ++counter; }
+Digit::Digit(const int x) : digit(x) { ++counter; }
+Digit::Digit(const double x) : digit(x) { ++counter; }
+Digit::Digit(const Digit& newDigit) { this->digit = newDigit.digit; ++counter; }
+Digit::Digit(Digit&& newDigit) {
+	this->digit = newDigit.digit;
+	newDigit.digit = 0;
+	++counter;
+}
+Digit::~Digit() {}
+
+int Digit::getDigit()const {
+	return digit;
+}
+int Digit::getCounter()const {
+	return Digit::counter;
+}
+void Digit::setDigit(int d) {
+	this->digit = d;
+}
+
+
+
+Digit Digit::operator-() {
+	Digit tmp;
+	tmp.digit = -(digit);
+	return tmp;
+}
+
+Digit Digit::operator+(const Digit& newDigit) {
+	Digit tmp = *this;
+	tmp += newDigit.digit;
+	return tmp;
+}
+Digit Digit::operator+(const int num) {
+	Digit tmp = *this;
+	tmp += num;
+	return tmp;
+}
+
+Digit Digit::operator-(const Digit& newDigit) {
+	Digit tmp = *this;
+	tmp -= newDigit.digit;
+	return tmp;
+}
+Digit Digit::operator-(const int num) {
+	Digit tmp = *this;
+	tmp -= num;
+	return tmp;
+}
+
+Digit Digit::operator*(const Digit& newDigit) {
+	Digit tmp = *this;
+	tmp *= newDigit.digit;
+	return tmp;
+}
+Digit Digit::operator*(const int num) {
+	Digit tmp = *this;
+	tmp *= num;
+	return tmp;
+}
+
+Digit Digit::operator/(const Digit& newDigit) {
+	Digit tmp = *this;
+	tmp /= newDigit.digit;
+	return tmp;
+}
+Digit Digit::operator/(const int num) {
+	Digit tmp = *this;
+	tmp /= num;
+	return tmp;
+}
+
+Digit Digit::operator%(const Digit& newDigit) {
+	Digit tmp = *this;
+	tmp %= newDigit.digit;
+	return tmp;
+}
+Digit Digit::operator%(const int num) {
+	Digit tmp = *this;
+	tmp %= num;
+	return tmp;
+}
+
+
+
+bool Digit::operator>(const Digit& newDigit) { return digit > newDigit.digit; }
+bool Digit::operator<(const Digit& newDigit) { return digit < newDigit.digit; }
+bool Digit::operator==(const Digit& newDigit) { return digit == newDigit.digit; }
+bool Digit::operator!=(const Digit& newDigit) { return digit != newDigit.digit; }
+bool Digit::operator>=(const Digit& newDigit) { return digit >= newDigit.digit; }
+bool Digit::operator<=(const Digit& newDigit) { return digit <= newDigit.digit; }
+
+Digit& Digit::operator=(const Digit& newDigit) {
+	if (this == &newDigit) return *this;
+	digit = newDigit.digit;
+	return *this;
+}
+Digit& Digit::operator++() {
+	++digit;
+	return *this;
+}
+Digit Digit::operator++(int) {
+	Digit tmp = *this;
+	++digit;
+	return tmp;
+}
+Digit& Digit::operator--() {
+	--digit;
+	return *this;
+}
+Digit Digit::operator--(int) {
+	Digit tmp = *this;
+	--digit;
+	return tmp;
+}
+
+Digit::operator int() { return digit; }
+Digit::operator double() { return (double)digit; }
+Digit::operator char() { return (char)digit; }
+
+void Digit::operator()(const int i) { digit = i; }
+
+
+
+Digit operator+(const int num, const Digit& dig1) {
+	return Digit(num + dig1.digit);
+}
+Digit operator-(const int num, const Digit& dig1) {
+	return Digit(num - dig1.digit);
+}
+Digit operator*(const int num, const Digit& dig1) {
+	return Digit(num * dig1.digit);
+}
+Digit operator/(const int num, const Digit& dig1) {
+	return Digit(num / dig1.digit);
+}
+
+
+
+std::ostream& operator<<(std::ostream& os, const Digit& a) {
+	os << a.digit;
+	return os;
+}
+std::istream& operator>>(std::istream& is, Digit& a) {
+	int tmp;
+	is >> tmp;
+	a.setDigit(tmp);
+	return is;
+}
+
+
+Digit& Digit::operator+=(const Digit& newDigit2){
+	digit += newDigit2.digit;
+	return *this;
+}
+Digit& Digit::operator+=(const int num){
+	digit += num;
+	return *this;
+}
+
+Digit& Digit::operator-=(const Digit& newDigit2){
+	digit -= newDigit2.digit;
+	return *this;
+}
+Digit& Digit::operator-=(const int num){
+	digit -= num;
+	return *this;
+}
+
+Digit& Digit::operator*=(const Digit& newDigit2){
+	digit *= newDigit2.digit;
+	return *this;
+}
+Digit& Digit::operator*=(const int num){
+	digit *= num;
+	return *this;
+}
+
+Digit& Digit::operator/=(const Digit& newDigit2){
+	digit /= newDigit2.digit;
+	return *this;
+}
+Digit& Digit::operator/=(const int num){
+	digit /= num;
+	return *this;
+}
+
+Digit& Digit::operator%=(const Digit& newDigit2){
+	digit %= newDigit2.digit;
+	return *this;
+}
+Digit& Digit::operator%=(const int num){
+	digit %= num;
+	return *this;
+}
---
+++ lab6/ex1/Digit.h
@@ -0,0 +1,79 @@
+#pragma once
+#include <iostream>
+class Digit
+{
+	int digit;
+	static unsigned int counter;
+
+public:
+	Digit();
+	explicit Digit(const int x);
+	explicit Digit(const double x);
+	Digit(const Digit&);
+	Digit(Digit&&);
+	~Digit();
+
+	int getDigit()const;
+	int getCounter()const;
+	void setDigit(int);
+
+	Digit operator-();
+
+	Digit operator+(const Digit&);
+	Digit operator+(const int);
+	
+	Digit operator-(const Digit&);
+	Digit operator-(const int);
+
+	Digit operator*(const Digit&);
+	Digit operator*(const int);
+
+	Digit operator/(const Digit&);
+	Digit operator/(const int);
+
+	Digit operator%(const Digit&);
+	Digit operator%(const int);
+
+	bool operator>(const Digit&);
+	bool operator<(const Digit&);
+	bool operator==(const Digit&);
+	bool operator!=(const Digit&);
+	bool operator>=(const Digit&);
+	bool operator<=(const Digit&);
+
+	Digit& operator=(const Digit&);
+	Digit& operator++();
+	Digit operator++(int);
+	Digit& operator--();
+	Digit operator--(int);
+
+	operator int();
+	operator double();
+	operator char();
+
+	void operator()(const int);
+
+
+	friend Digit operator+(const int, const Digit&);
+	friend Digit operator-(const int, const Digit&);
+	friend Digit operator*(const int, const Digit&);
+	friend Digit operator/(const int, const Digit&);
+
+
+	friend std::ostream& operator<<(std::ostream&, const Digit&);
+	friend std::istream& operator>>(std::istream&, Digit&);
+
+
+	Digit& operator+=(const Digit&);
+	Digit& operator+=(const int);
+	Digit& operator-=(const Digit&);
+	Digit& operator-=(const int);
+	Digit& operator*=(const Digit&);
+	Digit& operator*=(const int);
+	Digit& operator/=(const Digit&);
+	Digit& operator/=(const int);
+	Digit& operator%=(const Digit&);
+	Digit& operator%=(const int);
+
+};
+
