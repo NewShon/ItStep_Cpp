724a8fe8227f195d8cb653611c6fa44a9e2eba47
NewShon <kirill31_95@mail.ru>
2017-06-20 21:30:49 +0300

    Add files via upload

---
+++ lab_14/class_work/Main.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <map>
+#include<list>
+#include <set>
+
+
+void vect()
+{
+	std::ifstream in("input.txt");
+	std::vector<long long> myVector;
+
+	long long num;
+	while (in >> num)
+		myVector.push_back(num);
+
+
+	std::cout << myVector.size() << std::endl;
+	std::cout << myVector.capacity() << std::endl;
+
+
+	for (size_t i = 0; i < 50; ++i)
+		myVector.pop_back();
+
+	std::cout << std::endl;
+
+	std::cout << myVector.size() << std::endl;
+	std::cout << myVector.capacity() << std::endl;
+
+	myVector.shrink_to_fit();
+
+	std::cout << std::endl;
+
+	std::cout << myVector.size() << std::endl;
+	std::cout << myVector.capacity() << std::endl;
+
+
+	std::vector<long long> newVector;
+	for (size_t i = 0; i < myVector.size(); i += 2)
+		newVector.push_back(myVector[i]);
+
+
+	std::cout << std::endl;
+
+	newVector.shrink_to_fit();
+	std::cout << newVector.size() << std::endl;
+	std::cout << newVector.capacity() << std::endl;
+}
+void myList()
+{
+	std::ifstream in("input.txt");
+	std::list<int> myList;
+
+	for (int i = 10; i >= 0; --i)
+		myList.push_back(i);
+
+	myList.sort();
+	std::list<int>::iterator it = myList.begin();
+
+	++it;
+	myList.insert(it, 6);
+	myList.remove(1);
+
+	myList.sort();
+	myList.unique();
+
+
+	for (auto el : myList)
+		std::cout << el << " ";
+
+}
+
+
+
+int main(){
+
+	std::map<std::string,int> myMap;
+	myMap["q"] = 1;
+	myMap["w"] = 2;
+	myMap["e"] = 3;
+
+	std::cout << myMap["q"] << std::endl;
+	std::cout << myMap["e"] << std::endl;
+
+	return 0;
+}
