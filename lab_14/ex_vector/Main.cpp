ffa50daa4564db146b5b8ba92c2ffcf22d94d66b
NewShon <kirill31_95@mail.ru>
2017-06-21 19:30:08 +0300

    Add files via upload

---
+++ lab_14/ex_vector/Main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+
+
+int main() {
+
+	std::vector<int> myVec;
+	for (size_t i = 0; i < 99; i+=2)
+		myVec.push_back(i);
+
+	std::cout << "Size: " << myVec.size() << std::endl;
+	std::cout << "Capacity: " << myVec.capacity() << std::endl;
+
+	myVec[0] = myVec.size();
+
+	myVec.erase(myVec.begin() + 7);
+	myVec.erase(myVec.begin() + 13);
+	myVec.erase(myVec.begin() + 19);
+
+	size_t counter = 0;
+	for (size_t i = 0; i < myVec.size(); ++i)
+		if (myVec[i] % 4 == 0)
+			++counter;
+
+	std::cout << "Counter: " << counter << std::endl;
+
+	myVec[19] = 99;
+
+	myVec.pop_back();
+
+	myVec.back() = 101;
+
+	std::cout << "MyVec.size(): " << myVec.size() << std::endl;
+	for (auto el : myVec)
+		std::cout << el << " ";
+
+	std::cout << std::endl;
+	std::cout << std::endl;
+	
+	for (size_t i = 0; i < myVec.size(); ++i) {
+		myVec.erase(myVec.begin() + i);
+	}
+
+	for (auto el : myVec)
+		std::cout << el << " ";
+	std::cout << std::endl;
+
+	return 0;
+}
