21a1f67929b9477e4b9dbbbd6d5292041a36d76c
NewShon <kirill31_95@mail.ru>
2017-07-13 21:21:03 +0300

    Update Novo_Rectangle.h

--- lab_21/home_work/Novo_Rectangle.h
+++ lab_21/home_work/Novo_Rectangle.h
@@ -7,7 +7,13 @@ class Novo_Rectangle : public Novo_Parallelogram
 	double side2;
 public:
 	Novo_Rectangle(double s1, double s2) :
-		Novo_Parallelogram(s1, s2, 90), side1(s1), side2(s2) {}
+		Novo_Parallelogram(s1, s2, 90), side1(s1), side2(s2) 
+		{
+		if (side1 < 0 || side2 < 0) {
+			side1 = 0;
+			side2 = 0;
+		}
+	}
 	~Novo_Rectangle() {}
 
 	double getArea() {
