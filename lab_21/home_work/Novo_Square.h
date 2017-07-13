5296d20cd7d635b97070e2db0d3ce18721806629
NewShon <kirill31_95@mail.ru>
2017-07-13 21:19:50 +0300

    Update Novo_Square.h

--- lab_21/home_work/Novo_Square.h
+++ lab_21/home_work/Novo_Square.h
@@ -6,7 +6,11 @@ class Novo_Square : public Novo_Rectangle
 	double side1;
 public:
 	Novo_Square(double s1) :
-		Novo_Rectangle(s1, s1), side1(s1) {}
+		Novo_Rectangle(s1, s1), side1(s1){
+		if (side1 < 0) {
+			side1 = 0;
+		}
+	}
 	~Novo_Square() {}
 
 	double getArea() {
